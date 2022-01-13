545c4d38b3dd961d1ba778c1e5cd262dc56771a6
WMW-AR <97609599+WMW-AR@users.noreply.github.com>
2022-01-13 12:42:22 +0100

    pattern ratio to 0.75

--- threex/threex-artoolkitcontext.js
+++ threex/threex-artoolkitcontext.js
@@ -27,7 +27,7 @@ ARjs.Context = THREEx.ArToolkitContext = function(parameters){
 		canvasHeight: 480,
 
 		// the patternRatio inside the artoolkit marker - artoolkit only
-		patternRatio: 0.6,
+		patternRatio: 0.75,
 
 		// enable image smoothing or not for canvas copy - default to true
 		// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/imageSmoothingEnabled
